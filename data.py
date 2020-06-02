5bcf3c601702a41d9e9cc62362153fd90a650255
martabritala <53839047+martabritala@users.noreply.github.com>
2020-06-02 18:06:27 +0300

    ??????

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import os
 import psycopg2
+import json
 import csv
 import time
 
@@ -51,6 +52,9 @@ def nolasit(parametri = 0):
     if parametri==0:
         kverijs='''SELECT izvele.nrpk, izvele.vards, izvele.uzvards, izvele.datums, grozi.nosaukums, datori.datoru_nosaukums, padzilinatie1.padzkurss padzkurss1, padzilinatie2.padzkurss padzkurss2, padzilinatie3.padzkurss padzkurss3, izvele.specdebates, izvele.specanglit, izvele.specfiloz, izvele.specpub, izvele.specpapangv, izvele.specpsih, izvele.specrobo, izvele.speckrv FROM izvele LEFT JOIN grozi ON izvele.izvele_id=grozi.id LEFT JOIN datori ON izvele.datori_id=datori.id LEFT JOIN padzilinatie AS padzilinatie1 ON padz_id1=padzilinatie1.id LEFT JOIN padzilinatie AS padzilinatie2 ON padz_id2=padzilinatie2.id LEFT JOIN padzilinatie AS padzilinatie3 ON padz_id3=padzilinatie3.id ORDER BY nrpk ASC'''
         cur.execute(kverijs)
+        r = [dict((cur.description[i][0], value) \
+               for i, value in enumerate(row)) for row in cur.fetchall()]
+        return r
     elif parametri.izvele == 1:
         numurs = int(parametri.teksts)
         querry = '''SELECT * FROM izvele WHERE nrpk='{}' '''.format(numurs)
