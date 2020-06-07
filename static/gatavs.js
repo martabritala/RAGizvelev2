5294fd6f28f733f07aabb3de5df966828ef695f5
martabritala <53839047+martabritala@users.noreply.github.com>
2020-06-07 19:46:53 +0300

    ??

--- static/gatavs.js
+++ static/gatavs.js
@@ -2,6 +2,7 @@ class Gatavs {
     constructor(masivs){
         for (i=0;i<masivs.length;i++){
             document.getElementById("rez"+masivs[i]).style.display="hidden"
+            console.log(masivs[i])
         }
     }
 }
