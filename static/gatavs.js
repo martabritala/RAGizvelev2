3c4da30251404b2c61e31cff01ca1f67c7975f2a
martabritala <53839047+martabritala@users.noreply.github.com>
2020-06-09 20:17:33 +0300

    Valodas, forma?

--- static/gatavs.js
+++ static/gatavs.js
@@ -1,26 +1,34 @@
 class Gatavs {
-    constructor(masivs){
+    constructor(masivs, tresa, otra){
+        if(otra=='72'){
+            document.getElementById("rezotra73").style.display="hidden"
+        } else {
+            document.getElementById("rezotra72").style.display="hidden"
+        }
         for (let i=0;i<masivs.length;i++){
             if(masivs[i]=='19'){
-                
-            };  
-            if(masivs[i]=='-19'){
-
-            };
-            if(masivs[i]<0){
-                document.getElementById("rez"+(0-masivs[i])).style.display="none"
-            } else if (masivs[i]>0){
-                document.getElementById("rez"+masivs[i]).style.display="inherit"
+                if(tresa=='72'){
+                    document.getElementById("reztresa71").style.display="hidden"
+                } else {
+                    document.getElementById("reztresa72").style.display="hidden"
+                }
+            }else if(masivs[i]=='-19'){
+                document.getElementById("reztresa71").style.display="none";
+                document.getElementById("reztresa72").style.display="none";
+            }else if(masivs[i]<0){
+                document.getElementById("rez"+(0-masivs[i])).style.display="none";
+            }else if (masivs[i]>0){
+                document.getElementById("rez"+masivs[i]).style.display="inherit";
             } 
             console.log(masivs[i])
         }
     }
 }
 
-function palaist(jsonmasivs){
+function palaist(jsonmasivs, tresa, otra){
     console.log(jsonmasivs, "palaist")
     let masivs = jsonmasivs[0].split(",")
-    const g = new Gatavs(masivs)
+    const g = new Gatavs(masivs, tresa, otra)
 }
 
 function iesniegt(){
