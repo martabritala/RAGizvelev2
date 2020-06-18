e8f0290363280390405e4e1c91f6a683f06a25e3
martabritala <53839047+martabritala@users.noreply.github.com>
2020-06-18 12:35:04 +0300

    flex

--- static/gatavs.js
+++ static/gatavs.js
@@ -69,9 +69,9 @@ class Gatavs {
                     console.log(this.datori);
                 }
                 if (masivs[i]>=25 && masivs[i]<=32){
+                    document.getElementById("rez"+masivs[i]).style.display="flex";
                     if(this.padz1=="NULL"){
                         this.padz1=masivs[i]-24;
-                        console.log(this.padz1);
                     } else if (this.padz2=="NULL"){
                         this.padz2=masivs[i]-24;
                     } else if (this.padz3=="NULL"){
